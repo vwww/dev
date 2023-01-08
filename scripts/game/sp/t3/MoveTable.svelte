5a9623874149ef0c96f56cf36a12c1c7ba31e529
Victor <theonlypwner@users.noreply.github.com>
2023-01-08 14:33:30 -0700

    game/sp/t3: fix issues again

--- scripts/game/sp/t3/MoveTable.svelte
+++ scripts/game/sp/t3/MoveTable.svelte
@@ -17,6 +17,7 @@ const moves = [0, 1, 2, 3, 4, 5, 6, 7, 8]
   <thead>
     <tr>
       <th>Move</th>
+      <th>Paths</th>
       <th>Position</th>
       <th>X Win</th>
       <th>O Win</th>
--- scripts/game/sp/t3/MoveTableRow.svelte
+++ scripts/game/sp/t3/MoveTableRow.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import { MemoEntry, PathCount } from '@gc/t3/ai'
-import { winnerMapToNum, winnerMapInvert, Winner, WinnerMap, numToWinnerMap } from '@gc/t3/game'
+import { winnerMapToNum, Winner, WinnerMap } from '@gc/t3/game'
 import { GetMemoType } from './AppGameT3.svelte'
 
 export let i: number
@@ -19,9 +19,9 @@ function remapPathCount (p: PathCount, winnerMap: WinnerMap): PathCount {
   return result
 }
 
-function memoTitle (memo: MemoEntry, gameType: number): string {
+function memoTitle (memo: MemoEntry, winnerMap: WinnerMap): string {
   const countPathsRaw = memo[1]
-  const countPaths = remapPathCount(countPathsRaw, numToWinnerMap(gameType))
+  const countPaths = remapPathCount(countPathsRaw, winnerMap)
 
   return `Path Count:\n\n${formatPathCount(countPaths)}\n\nUnmapped\n${formatPathCount(countPathsRaw)}`
 
@@ -35,14 +35,14 @@ function memoTitle (memo: MemoEntry, gameType: number): string {
 
 type Entry = [text: string, className?: string, title?: string]
 
-function winInfo (memo: MemoEntry, result: number, gameType2: number): Entry {
+function winInfo (memo: MemoEntry, result: number, winnerMap2: WinnerMap): Entry {
   const [memoEntry, countPathsRaw] = memo
-  const countPaths = remapPathCount(countPathsRaw, numToWinnerMap(gameType))
+  const countPaths = remapPathCount(countPathsRaw, winnerMap)
   const countPathPossible = countPaths[result]
 
   const canDo = countPathPossible
   const onlyPossible = canDo && !((result !== 1 && countPaths[1]) || (result !== 2 && countPaths[2]) || (result !== 3 && countPaths[3]))
-  const entry = memoEntry[gameType2]
+  const entry = memoEntry[winnerMapToNum(winnerMap2)]
 
   const val = entry[0]
   const canForce = val > 0 ? (10 - val) : 0
@@ -51,7 +51,7 @@ function winInfo (memo: MemoEntry, result: number, gameType2: number): Entry {
     canForce ? `${onlyPossible ? 'Result' : 'Forcable'} (${canForce === i + 1 ? 'now' : 'by #' + canForce})`
       : canDo ? val < 0 ? 'Preventable' : 'Possible' : 'Impossible'
 
-  const className = `table-${canForce ? 'info' : canDo ? val < 0 ? 'warning' : 'success' : 'danger'}`
+  const className = `table-${canForce ? 'info' : canDo ? 'success' : 'danger'}`
 
   let title: string | undefined
   if (countPathPossible) {
@@ -75,19 +75,21 @@ const emptyInfo = [emptyEntry, emptyEntry, emptyEntry]
 $: made = moveLength > i
 $: hasInfo = made || (moveLength === i && !winner)
 $: memo = hasInfo && getMemo ? getMemo(boardHistory[i]) : undefined
-$: gameType = winnerMapToNum(winnerMap)
-$: gameTypeInv = winnerMapToNum(winnerMapInvert(winnerMap))
-$: gameTypeTie = winnerMapToNum(winnerMap.map(w => ((i ^ (w === 3 ? 0 : 1)) & 1) + 1) as WinnerMap)
+
+$: winnerMapX = winnerMap.map(w => ((i ^ (w === Winner.P1 ? 0 : 1)) & 1) + 1) as WinnerMap
+$: winnerMapO = winnerMap.map(w => ((i ^ (w === Winner.P2 ? 0 : 1)) & 1) + 1) as WinnerMap
+$: winnerMapTie = winnerMap.map(w => ((i ^ (w === Winner.Tie ? 0 : 1)) & 1) + 1) as WinnerMap
 $: info = !memo ? emptyInfo
   : [
-    winInfo(memo, 1, (i & 1) ? gameTypeInv : gameType),
-    winInfo(memo, 2, (i & 1) ? gameType : gameTypeInv),
-    winInfo(memo, 3, gameTypeTie)
+    winInfo(memo, 1, winnerMapX),
+    winInfo(memo, 2, winnerMapO),
+    winInfo(memo, 3, winnerMapTie)
   ]
 </script>
 
 <tr>
-  <td title={memo && memoTitle(memo, gameType)}>#{i + 1}</td>
+  <td>#{i + 1}</td>
+  <td title={memo && memoTitle(memo, winnerMap)}>{memo?.[1][0].toLocaleString() ?? ''}</td>
   <td class:table-warning={made}>{made ? moveStack[i] + 1 : ''}</td>
   {#each info as c}
     <td class={c[1]} title={c[2]}>{c[0]}</td>
