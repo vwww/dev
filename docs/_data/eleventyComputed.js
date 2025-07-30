56d3c6b825fe01c907dce501e4eea9ec98916f8e
Victor <theonlypwner@users.noreply.github.com>
2025-07-30 14:51:01 -0600

    Fix page_url with 11ty on Windows

--- docs/_data/eleventyComputed.js
+++ docs/_data/eleventyComputed.js
@@ -1,5 +1,5 @@
 export default {
-	page_url: (data) => data.page.url.replace(/(index)?\.html$/, ''),
+	page_url: (data) => data.page.url.replace(/^\/+/, '/').replace(/(index)?\.html$/, ''),
 	page_path: (data) => data.page.inputPath.slice(data.eleventy.directories.input.length),
 	page_level: (data) => data.page_url.split('/').length - 2, // subtract leading slash
 	base: (data) => Array(data.page_level).fill('..').join('/'),
