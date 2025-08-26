25cd9a81479da1ae13743928ef6fdd2f29efaab1
Victor <theonlypwner@users.noreply.github.com>
2025-08-25 20:53:33 -0600

    game/mp: show min count as 1/32 of bar

--- scripts/game/mp/common/CardCountBars.svelte
+++ scripts/game/mp/common/CardCountBars.svelte
@@ -8,7 +8,8 @@ const { ranks, cards }: Props = $props()
 </script>
 
 {#if cards.at(-1)}
-  {@const max = BigInt(cards.slice(0, -1).reduce((v, cur) => (v > cur ? v : cur)))}
+  {@const min = BigInt(cards.filter((x) => x).reduce((v, cur) => (v < cur ? v : cur)))}
+  {@const range = BigInt(cards.slice(0, -1).reduce((v, cur) => (v > cur ? v : cur))) - min}
   <table class="table table-sm">
     <tbody>
       {#each cards as c, i}
@@ -16,9 +17,20 @@ const { ranks, cards }: Props = $props()
           <tr>
             <td>{ranks[i]}</td>
             <td class="w-100">
-              <div class="progress my-1">
-                <div class="progress-bar" style="width: {Number(BigInt(c) * 100000n / max) / 1000}%"></div>
-              </div>
+              {#if range}
+                <div class="progress-stacked my-1">
+                  <div class="progress" style="width: 3.125%">
+                    <div class="progress-bar bg-info"></div>
+                  </div>
+                  {#if c > min}
+                    <div class="progress" style="width:{Number((BigInt(c) - min) * 96875n / range) / 1000}%">
+                      <div class="progress-bar"></div>
+                    </div>
+                  {/if}
+                </div>
+              {:else}
+                <div class="progress bg-primary"></div>
+              {/if}
             </td>
             <td>{c}</td>
           </tr>
