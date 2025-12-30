4971da6d320ce97f6be29ee1fa43a91548dbf0f6
Victor <theonlypwner@users.noreply.github.com>
2025-12-29 17:32:13 -0700

    fix(game/mp/slime): use consistent option names

--- scripts/game/mp/slime/SlimeGame.svelte.ts
+++ scripts/game/mp/slime/SlimeGame.svelte.ts
@@ -166,9 +166,9 @@ export class SlimeGame extends RealTimeGame<SlimeClient> {
   protected override processWelcomeMode (m: ByteReader): void {
     this.mode.optServe = m.get() & 3 // 2 bits exactly
     this.mode.optIntermission = Math.min(Number(m.getUint64()), 3000)
-    this.mode.optGameSpeed = clamp(m.get(), 25, 800)
-    this.mode.optPlayerSpeed = clamp(m.get(), 25, 800)
-    this.mode.optBallSpeed = clamp(m.get(), 10, 800)
+    this.mode.optSpeedGame = clamp(m.get(), 25, 800)
+    this.mode.optSpeedPlayer = clamp(m.get(), 25, 800)
+    this.mode.optSpeedBall = clamp(m.get(), 10, 800)
     this.mode.optGravity = clamp(m.get(), 10, 800)
   }
 
--- scripts/game/mp/slime/gamemode.ts
+++ scripts/game/mp/slime/gamemode.ts
@@ -9,9 +9,9 @@ export const roomCreateOptions = [
     'random'
   ]],
   ['optIntermission', 'i', 1000, 'Intermission Time / ms', 'time after round ends, in milliseconds', 0, 3000],
-  ['optGameSpeed', 'i', 100, 'Game Speed / %', 'game speed multiplier', 25, 800],
-  ['optPlayerSpeed', 'i', 100, 'Player Speed / %', 'player speed multiplier', 25, 800],
-  ['optBallSpeed', 'i', 100, 'Ball Speed / %', 'ball speed multiplier', 10, 800],
+  ['optSpeedGame', 'i', 100, 'Game Speed / %', 'game speed multiplier', 25, 800],
+  ['optSpeedPlayer', 'i', 100, 'Player Speed / %', 'player speed multiplier', 25, 800],
+  ['optSpeedBall', 'i', 100, 'Ball Speed / %', 'ball speed multiplier', 10, 800],
   ['optGravity', 'i', 100, 'Gravity / %', 'gravity multiplier', 10, 800],
 ] as const
 
@@ -24,8 +24,8 @@ export function defaultMode (): SlimeMode {
 export function getGameModeString (mode: SlimeMode): string {
   return `${['alternate', 'winner', 'loser', 'random'][mode.optServe]} serve`
     + `, ${mode.optIntermission / 1000}s intermission`
-    + `, (${mode.optGameSpeed / 100}x game`
-    + `, ${mode.optPlayerSpeed / 100}x player`
-    + `, ${mode.optBallSpeed / 100}x ball) speed`
+    + `, (${mode.optSpeedGame / 100}x game`
+    + `, ${mode.optSpeedPlayer / 100}x player`
+    + `, ${mode.optSpeedBall / 100}x ball) speed`
     + `, ${mode.optGravity / 100}x gravity`
 }
