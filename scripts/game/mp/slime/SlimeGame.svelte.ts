b3efeab235330f830c2675f37c40a2a23aed32ab
Victor <theonlypwner@users.noreply.github.com>
2026-01-03 11:26:53 -0700

    feat(game/mp/slime): show "Game starting" status

--- scripts/game/mp/slime/SlimeGame.svelte.ts
+++ scripts/game/mp/slime/SlimeGame.svelte.ts
@@ -152,6 +152,7 @@ export class SlimeGame extends RealTimeGame<SlimeClient> {
 
     this.p1.score = this.p2.score = 0n
     this.gameStart = Date.now()
+    this.status = GameStatus.INIT
   }
 
   protected override playerDeactivated (player: SlimeClient): void {
@@ -163,6 +164,7 @@ export class SlimeGame extends RealTimeGame<SlimeClient> {
 
     this.p1.score = this.p2.score = 0n
     this.gameStart = Date.now()
+    this.status = GameStatus.INIT
 
     super.playerDeactivated(player)
   }
@@ -309,7 +311,8 @@ export class SlimeGame extends RealTimeGame<SlimeClient> {
 const enum GameStatus {
   IN_PLAY,
   WIN,
-  LOSS
+  LOSS,
+  INIT,
 }
 
 // only used by UI
@@ -433,11 +436,15 @@ export class GameDrawer {
       if (!this.game.localClient.active) {
         status = !this.game.status
           ? 'Spectating'
-          : `${this.game.status === GameStatus.WIN !== this.game.flipP1 ? 'Left' : 'Right'} wins the round!`
+          : this.game.status === GameStatus.INIT
+            ? 'Game starting'
+            : `${this.game.status === GameStatus.WIN !== this.game.flipP1 ? 'Left' : 'Right'} wins the round!`
       } else if (this.game.status === GameStatus.WIN) {
         status = 'You won the round!'
       } else if (this.game.status === GameStatus.LOSS) {
         status = 'You lost the round!'
+      } else if (this.game.status === GameStatus.INIT) {
+        status = 'Game starting'
       }
       const statusTime = `${!this.game.status ? ((Date.now() - this.game.roundStart) / 1000).toFixed(1) + '/' : ''}${((Date.now() - this.game.gameStart) / 1000).toFixed(1)}`
       status = status ? `${status} (${statusTime})` : statusTime
