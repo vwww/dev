adc18013380d46e2a850c65b098c8120c7a6cb66
Victor <theonlypwner@users.noreply.github.com>
2026-03-15 08:57:17 -0600

    feat(bots): show full tweet with checkbox or hover

--- scripts/bots/random/TweetList.svelte
+++ scripts/bots/random/TweetList.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+import { pState } from '@/util/svelte.svelte'
 import { slide } from 'svelte/transition'
 
 const TWEET_LEN = 280
@@ -8,24 +9,49 @@ interface Props {
 }
 
 const { tweets }: Props = $props()
+
+let showFullTweet = pState('game/mp/_shared/showFullTweet', false)
 </script>
+
+<div class="input-group mb-3">
+  <span class="input-group-text flex-grow-1">
+    <label class="form-check mx-auto">
+      <input type="checkbox" class="form-check-input" bind:checked={showFullTweet.value}>
+      Show full tweet text
+    </label>
+  </span>
+</div>
 
 <div>
   {#each tweets as [tweetShort, tweetFull], i (i + tweetShort)}
-    <div class="t my-2 px-3 py-2" transition:slide title={tweetFull}>
+    <div class="tweet my-2 px-3 py-2" class:showFull={showFullTweet.value} transition:slide title={tweetFull}>
       {#if tweetFull}
         <span class="badge text-bg-danger">{tweetFull.length}</span>
       {:else}
         <span class="badge text-bg-{tweetShort.length === TWEET_LEN ? 'success' : 'warning'}">{tweetShort.length}</span>
       {/if}
-      {tweetShort}
+      <span class="body" data-short={tweetShort} data-full={tweetFull || tweetShort}></span>
     </div>
   {/each}
 </div>
 
 <style>
-.t {
+.tweet {
   border: 1px dashed;
   font-size: 1.6em;
+
+  .body::after {
+    content: attr(data-short);
+  }
+
+  &:hover, &:active, &:focus {
+    border: 1px solid;
+  }
+
+  &.showFull, &:hover, &:active, &:focus {
+    .body::after {
+      content: attr(data-full);
+    }
+  }
 }
 </style>
